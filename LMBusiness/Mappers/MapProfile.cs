f5088dd92e22a98a31eb202ca8307c9e0a8d48e9
Abdulhadi Fırat <abdulhadi.firat@gmail.com>
2025-08-31 19:48:34 +0300

    Mapping done

---
+++ LMBusiness/Mappers/MapProfile.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using LM.Entity.Concrete;
+using LM.Entity.DTOs.Book;
+using LM.Entity.DTOs.Loan;
+using LM.Entity.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM.Business.Mappers;
+
+public class MapProfile : Profile
+{
+    public MapProfile()
+    {
+        CreateMap<Book, BookResponseDto>();
+        CreateMap<Book, BookDetailResponseDto>();
+        CreateMap<BookCreateRequestDto, Book>();
+        CreateMap<BookUpdateRequestDto, Book>();
+
+        CreateMap<User, UserResponseDto>(); 
+        CreateMap<User, UserDetailResponseDto>();
+        CreateMap<UserCreateRequestDto, User>();
+        CreateMap<UserUpdateRequestDto, User>();
+
+        CreateMap<Loan, LoanResponseDto>();
+        CreateMap<Loan, LoanDetailResponseDto>();
+        CreateMap<LoanCreateRequestDto, Loan>();
+        CreateMap<LoanUpdateRequestDto, Loan>();
+        
+    }
+}
