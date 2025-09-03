44f1b59e19c047eb1840b18a00237742cabce0e4
Abdulhadi Fırat <abdulhadi.firat@gmail.com>
2025-09-03 20:53:25 +0300

    Autofac completed

---
+++ LMBusiness/DependencyResolver/AutoFac/AutofacBusinessModule.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Core.UnitOfWorks;
+using LM.Business.Abstract;
+using LM.Business.Concrete;
+using LM.DataAccess.Abstract;
+using LM.DataAccess.Concrete.EntityFramework;
+using LM.DataAccess.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LM.Business.DependencyResolver.AutoFac;
+
+public class AutofacBusinessModule :Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<EfBookRepo>().As<IBookRepo>();
+        builder.RegisterType<BookManager>().As<IBookServ>();
+
+        builder.RegisterType<EfUserRepo>().As<IUserRepo>();
+        builder.RegisterType<UserManager>().As<IUserServ>();
+
+        builder.RegisterType<EfLoanRepo>().As<ILoanRepo>();
+        builder.RegisterType<LoanManager>().As<ILoanServ>();
+
+        builder.RegisterType<UnitOfWork>().As<IUnitOfWork>();
+    }
+}
